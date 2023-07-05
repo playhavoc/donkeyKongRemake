5b48d1ed0d73aaa16692e7d5a7962b75a1b6b8ba
Linus Sharp <linuscolesharp@gmail.com>
2023-07-06 00:34:02 +0100

    feat: added moving and jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float moveForce = 10f;
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    private float movementX;
+
+    private Rigidbody2D myBody;
+
+    private void Awake()
+    {
+        myBody= GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +27,25 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerMoveKeyboard();
+    }
+
+    private void FixedUpdate()
+    {
+        jump();
+    }
+
+    void playerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            myBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
     }
 }
